ccd42531f2be52a80662e42e97ef2a19d07ae7c0
Sabrineh <Sabrinehmokhtari@yahoo.com>
2021-04-16 22:56:55 +0430

    added forms

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from .models import *
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    mobile = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'mobile']
+
+
+class ProfileForm(ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['type', 'password']
